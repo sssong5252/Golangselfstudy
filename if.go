98448dd5ca76efb296ecb911845c26726c16a257
sssong5252 <luckyjude@naver.com>
2024-01-15 18:03:48 +0900

    0115

--- if.go
+++ if.go
@@ -6,11 +6,13 @@ func main() {
 	var i int
 	fmt.Print("num = ")
 	fmt.Scanln(&i)
-	fmt.Println("num is ", i)
+	fmt.Println("num is", i)
 
 	if i == 4 {
 		fmt.Print("the num is ", i)
+	} else if i > 10{
+		fmt.Println("too big")
 	} else {
-		fmt.Println("not correct")
+		fmt.Println("incorrect")
 	}
 }
