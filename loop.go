dda154cdfda8e0c9da569dcc374606a6750b662e
sssong5252 <luckyjude@naver.com>
2024-01-20 15:54:42 +0900

    0120

--- loop.go
+++ loop.go
@@ -1,11 +1,13 @@
 package main
+
 import "fmt"
-func main()
-{
-	fmt.Scanln(var i)
+
+func main() {
+	var i int
+	fmt.Scanln(&i)
 
 	for i := 0; i < 10; i++ {
-		if (i == 3) {
+		if i == 3 {
 			fmt.Println("i am 3")
 		}
 		fmt.Println("hello")
