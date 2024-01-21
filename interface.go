15ae9afb1da6c42b83b30fb38564b6b9f304a987
sssong5252 <luckyjude@naver.com>
2024-01-21 13:01:13 +0900

    0121

--- interface.go
+++ interface.go
@@ -3,6 +3,8 @@ import "fmt"
 
 func main() {
 	var s sampleinterface //interface 변수에 값이 설정되어 있지 않아서 런타임 에러(nil 포인터 에러)
+	//nil = null instance pointer (if method have no return value, just pass )
+	//if method have return value, return value is 0 or nil
 	fmt.Println(s)
 	s.samplemethod()
 }
